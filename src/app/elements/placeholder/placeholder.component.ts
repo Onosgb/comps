b823e5f4dd62deb48e9196e6eaa7ec597353298a
Onoriode Godbless <onosgb@gmail.com>
2021-10-25 16:44:25 +0100

    first upload
    Small component based app

---
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.css'],
+})
+export class PlaceholderComponent implements OnInit {
+  @Input() header: boolean = true;
+  @Input() lines: number = 5;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/mods/modal/modal.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  constructor(private el: ElementRef) {}
+  @Output() close = new EventEmitter();
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  onCloseClick() {
+    this.close.emit(false);
+  }
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+}
